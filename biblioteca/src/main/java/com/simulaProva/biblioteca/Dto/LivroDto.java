c111fa11a49a3d6c7639331de7eef6cc72cb0705
Gabriel-August <gabrielasnc55@gmail.com>
2025-09-30 14:57:34 -0300

    dto

--- biblioteca/src/main/java/com/simulaProva/biblioteca/Dto/LivroDto.java
+++ biblioteca/src/main/java/com/simulaProva/biblioteca/Dto/LivroDto.java
@@ -1,6 +1,32 @@
 package com.simulaProva.biblioteca.Dto;
 
-public class LivroDto {
+import com.simulaProva.biblioteca.Entity.Livro;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-    //Dto
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class LivroDto {  //Dto
+
+ private long id;
+ private String titulo;
+ private String autor;
+ private String editora;
+ private int anoDePublicacao;
+ private BigDecimal preco;
+
+ public LivroDto(Livro entidade){
+  this.id = entidade.getId();
+  this.titulo = entidade.getTitulo();
+  this.autor = entidade.getAutor();
+  this.editora = entidade.getEditora();
+  this.preco = entidade.getPreco();
+ }
+
+
 }
