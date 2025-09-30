a74e297e2af40d37df1e31296e726d223c21863b
DiogoCamps <diogoapferreiracampos@gmail.com>
2025-09-30 14:41:42 -0300

    LivroRepository

--- biblioteca/src/main/java/com/simulaProva/biblioteca/Repository/LivroRepository.java
+++ biblioteca/src/main/java/com/simulaProva/biblioteca/Repository/LivroRepository.java
@@ -1,5 +1,11 @@
 package com.simulaProva.biblioteca.Repository;
 
-public interface LivroRepository {
-    //Repository
+import com.simulaProva.biblioteca.Entity.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface LivroRepository extends JpaRepository <Livro, Long>   {
+    //Implementação de buscas de livros por título
+    List<Livro> findByTituloContainingIgnoreCase(String titulo);
 }
